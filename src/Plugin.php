fa52c391f42bd89a96158428029d11a98fe127ba
Daniel Hürtgen <daniel@higidi.com>
2017-03-31 23:11:30 +0200

    TASK: Composer plugin initialized

---
+++ src/Plugin.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Higidi\ComposerPhpCSStandardsPlugin;
+
+use Composer\Composer;
+use Composer\IO\IOInterface;
+use Composer\Plugin\PluginInterface;
+
+class Plugin implements PluginInterface
+{
+    public function activate(Composer $composer, IOInterface $io)
+    {
+        // TODO: Implement activate() method.
+    }
+}
