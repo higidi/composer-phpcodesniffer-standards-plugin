9952aa3d94bdd1870eca9d32a449a3eced849315
Daniel Hürtgen <daniel@higidi.de>
2017-04-15 23:11:28 +0200

    FEATURE: Implement standard factory

---
+++ tests/src/PHPCodeSniffer/Standards/Higidi/ComposerPhpCSStandardsPlugin/Tests/PHPCodeSniffer/Standards/FactoryTest.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace Higidi\ComposerPhpCSStandardsPlugin\Tests\PHPCodeSniffer\Standards;
+
+/*
+ * Copyright (C) 2017  Daniel Hürtgen <daniel@higidi.de>
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+ * 02110-1301, USA.
+ */
+
+use Higidi\ComposerPhpCSStandardsPlugin\PHPCodeSniffer\Standard;
+use Higidi\ComposerPhpCSStandardsPlugin\PHPCodeSniffer\Standards\Factory;
+
+/**
+ * Test case for class \Higidi\ComposerPhpCSStandardsPlugin\PHPCodeSniffer\Standards\Factory
+ */
+class FactoryTest extends \PHPUnit_Framework_TestCase
+{
+    /**
+     * @var Factory
+     */
+    protected $fixture;
+
+    protected function setUp()
+    {
+        parent::setUp();
+        $this->fixture = new Factory();
+    }
+
+    protected function tearDown()
+    {
+        parent::tearDown();
+        unset($this->fixture);
+    }
+
+    public function testCreateStandard()
+    {
+        $standard = $this->fixture->create('foo');
+
+        $this->assertInstanceOf(Standard::class, $standard);
+    }
+}
