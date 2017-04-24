01a25c721f26ac8d84a4b2fb1bc3525efd7a2f8b
Daniel Hürtgen <daniel@higidi.de>
2017-04-24 09:58:02 +0200

    FEATURE: Implement finder service

---
+++ src/PHPCodeSniffer/Finder.php
@@ -0,0 +1,77 @@
+<?php
+
+namespace Higidi\ComposerPhpCSStandardsPlugin\PHPCodeSniffer;
+
+/*
+ * Copyright (C) 2017  Daniel Hürtgen <daniel@higidi.de>
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+ * 02110-1301, USA.
+ */
+
+use Symfony\Component\Finder\Finder as SymfonyFinder;
+use Symfony\Component\Finder\SplFileInfo;
+
+/**
+ * Standards finder class.
+ */
+class Finder
+{
+
+    /**
+     * @var StandardsFactory
+     */
+    protected $factory;
+
+    /**
+     * @param SymfonyFinder|null $finder
+     * @param StandardsFactory|null $factory
+     */
+    public function __construct(StandardsFactory $factory = null)
+    {
+        $this->factory = $factory ?: new StandardsFactory();
+    }
+
+    /**
+     * Find and return PHPCodeSniffer standards.
+     *
+     * @param string $path
+     * @return Standards
+     */
+    public function in($path)
+    {
+        $finder = SymfonyFinder::create()->files()->in($path)->name('ruleset.xml');
+        $paths = iterator_to_array($finder, false);
+        $paths = array_map(
+            function (SplFileInfo $file) {
+                return $file->getPath();
+            },
+            $paths
+        );
+
+        return $this->createStandardsFromPath($paths);
+    }
+
+    /**
+     * Creates PHPCodeSniffer standards from paths.
+     *
+     * @param array $path
+     * @return Standards
+     */
+    protected function createStandardsFromPath(array $paths)
+    {
+        return $this->factory->create($paths);
+    }
+}
---
+++ tests/src/PHPCodeSniffer/FinderTest.php
@@ -0,0 +1,77 @@
+<?php
+
+namespace Higidi\ComposerPhpCSStandardsPlugin\Tests\PHPCodeSniffer\Standard;
+
+/*
+ * Copyright (C) 2017  Daniel Hürtgen <daniel@higidi.de>
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+ * 02110-1301, USA.
+ */
+
+use Higidi\ComposerPhpCSStandardsPlugin\PHPCodeSniffer\Finder;
+use Higidi\ComposerPhpCSStandardsPlugin\PHPCodeSniffer\StandardsFactory;
+
+/**
+ * Test case for class \Higidi\ComposerPhpCSStandardsPlugin\PHPCodeSniffer\Finder
+ */
+class FinderTest extends \PHPUnit_Framework_TestCase
+{
+    /**
+     * @var Finder
+     */
+    protected $fixture;
+
+    /**
+     * @var StandardsFactory|\PHPUnit_Framework_MockObject_MockObject
+     */
+    protected $standardsFactoryMock;
+
+    protected function setUp()
+    {
+        parent::setUp();
+        $this->standardsFactoryMock = $this->getMock(
+            'Higidi\ComposerPhpCSStandardsPlugin\PHPCodeSniffer\StandardsFactory',
+            array(),
+            array(),
+            '',
+            false
+        );
+        $this->fixture = new Finder($this->standardsFactoryMock);
+    }
+
+    protected function tearDown()
+    {
+        parent::tearDown();
+        unset($this->fixture);
+        unset($this->standardsFactoryMock);
+    }
+
+    public function testFinder()
+    {
+        $path = realpath(implode(DIRECTORY_SEPARATOR, array(__DIR__, '..', '..', 'Fixtures', 'Standards')));
+        $expected = array(
+            $path . DIRECTORY_SEPARATOR . 'Standard1',
+            $path . DIRECTORY_SEPARATOR . 'Standard2',
+            $path . DIRECTORY_SEPARATOR . 'Standard3',
+        );
+
+        $this->standardsFactoryMock
+            ->expects($this->once())
+            ->method('create')
+            ->with($expected);
+        $this->fixture->in($path);
+    }
+}
